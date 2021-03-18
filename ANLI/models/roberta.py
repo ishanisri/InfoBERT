e975a8f79887fba66d892bbf9bd19d444f51be92
ishanisri <ishasri.1212@gmail.com>
2021-03-18 12:30:54 +0530

    check config

--- ANLI/models/roberta.py
+++ ANLI/models/roberta.py
@@ -306,7 +306,7 @@ class RobertaForSequenceClassification(BertPreTrainedModel):
         self.num_labels = config.num_labels
         self.roberta = RobertaModel(config)
 
-        print(config)
+        logger.info(config)
 
         # For TextHide
         #self.num_sigma = config.num_sigma
